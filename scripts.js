567093abc8848fbb8e7d31571fa55fd6ac16f172
aditihnayak <nayakaditi203@gmail.com>
2024-11-23 22:34:09 +0530

    Login Validation

--- scripts.js
+++ scripts.js
@@ -2,9 +2,20 @@
 function validateLogin() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
+
     if (username && password) {
-        window.location.href = "subjects.html";
+        if (
+            (username === "nnm23cs293@nmamit.in" && password === "nnm23cs293") ||
+            (username === "nnm23cs227@nmamit.in" && password === "nnm23cs227") ||
+            (username === "nnm23is152@nmamit.in" && password === "nnm23is152") ||
+            (username === "nnm23is212@nmamit.in" && password === "nnm23is212")
+        ) {
+            window.location.href = "subjects.html";
+        } else {
+            alert("Incorrect Username or Password");
+        }
     } else {
-        alert("Please enter both username and password.");
+        alert("Please Enter Both Username and Password");
     }
 }
+
